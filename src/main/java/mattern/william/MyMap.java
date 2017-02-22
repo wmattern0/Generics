4f816d8b6c959afcafe7e3795e7106670441ac84
wmattern0 <wm.m.mattern@gmail.com>
2017-02-22 18:07:32 -0500

    My map interface started

--- src/main/java/mattern/william/MyMap.java
+++ src/main/java/mattern/william/MyMap.java
@@ -2,7 +2,23 @@ package mattern.william;
 
 public class MyMap<K, V> {
 
+    protected MyMapNode<K, V> start;
+    protected MyMapNode<K, V> end;
+    private int size;
 
+    public MyMap(){
+        start = null;
+        end = null;
+        size = 0;
+    }
+
+    public static class MyMapNode<K,V>{
+        private K key;
+        private V value;
+        private MyMapNode next;
+
+
+    }
 
 
 
